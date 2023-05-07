e52758346b7d613b75e51e414c4e316b1694d6cc
Anh Pham <115387558+aphamtv@users.noreply.github.com>
2023-05-07 16:08:18 +0200

    titanic

---
+++ README.md
@@ -0,0 +1,8 @@
+# Docker Exercise
+## Contents
+This repository contains the following files:
+
+* `hello.html`: Contains a simple HTML page that displays a welcome message and an image.
+* `titanic.html`: Contains an HTML page that displays a table of the Titanic dataset using Pandas.
+### About
+This repository was created as part of the Docker exercise for the Big Data course in the BIPM program at HWR Berlin. The hello.html file is a basic HTML page that demonstrates how to display an image in a Flask app using the url_for function. The titanic.html file displays a table of the Titanic dataset using Pandas.
--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 import time
 import redis
-from flask import Flask, render_template
+import pandas as pd
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+app.config['IMAGE_FOLDER'] = 'static/images'
+
 cache = redis.Redis(host='redis', port=6379)
+app.use_static = True
 
 def get_hit_count():
     retries = 5
@@ -19,7 +23,19 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return render_template('hello.html', name= "BIPM", count = count)
+    image_url = url_for('static', filename='docker_image.jpg')
+    return render_template('hello.html', name='BIPM', count=count, image_url=image_url)
+
+@app.route('/titanic')
+def titanic():
+    # Load the Titanic dataset using Pandas
+    df = pd.read_csv('templates/titanic.csv')
+    
+    # Convert the first 5 rows of the DataFrame to an HTML table
+    table_html = df.head().to_html()
+    
+    # Render the template with the table data
+    return render_template('titanic.html', table_data=table_html)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
